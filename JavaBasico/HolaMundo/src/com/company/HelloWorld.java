b129386f494279da335e68881f84fdaa2d522a08
esteban-90 <estebanvm1990@gmail.com>
2022-05-21 17:21:23 -0400

    Entrega ejercicios tema 1

---
+++ JavaBasico/HolaMundo/src/com/company/HelloWorld.java
@@ -0,0 +1,46 @@
+package com.company;
+
+/**
+ * Clase principal
+ *
+ * @author Esteban V.M.
+ */
+public class HelloWorld {
+    /**
+     * Función principal de entrada a la aplicación.
+     *
+     * @param args argumentos a recibir.
+     * @since 1.0
+     */
+    public static void main (String[] args) {
+        /*
+        Comentario
+         */
+        System.out.println("Hola mundo desde clase Hello World.");
+
+        // enteros
+        byte numer1 = 1;
+        short number2 = 2;
+        int number3 = 3;
+        long number4 = 4l;
+
+        // punto flotante
+        float decimal1 = 4.9f;
+        double decimal2 = 9.9d;
+
+        // caracter
+        char caracter1 = 'a';
+
+        // booleanos
+        boolean verdadero = true;
+        boolean falso = false;
+
+        // texto
+        String nombre = "nombre";
+        String apellido = "apellido";
+
+        // envoltorio
+        Integer numero = null;
+        Long numero2 = 5L;
+    }
+}
---
+++ JavaBasico/Tema1/src/com/example/Tema1.java
@@ -0,0 +1,39 @@
+package com.example;
+
+public class Tema1 {
+    public static void main (String[] args) {
+        // 1. Numéricos:
+
+        // 1.1 Enteros:
+        byte variable1 = 5;
+        short variable2 = 10;
+        int variable3 = 30;
+        long variable4 = 100;
+
+        System.out.println(variable1);
+        System.out.println(variable2);
+        System.out.println(variable3);
+        System.out.println(variable4);
+
+        // 1.2 Decimales:
+        float variable5 = 5.5f;
+        double variable6 = 10.5d;
+
+        System.out.println(variable5);
+        System.out.println(variable6);
+
+        // 2. Booleanos:
+        boolean variable7 = false;
+        boolean variable8 = true;
+
+        System.out.println(variable7);
+        System.out.println(variable8);
+
+        // 3. Texto:
+        char variable9 = 'a';
+        String variable10 = "Lorem ipsum...";
+
+        System.out.println(variable9);
+        System.out.println(variable10);
+    }
+}
